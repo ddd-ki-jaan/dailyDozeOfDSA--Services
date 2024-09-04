c1358fb8c443de456c33e4923270fb7c64f71572
mdsaquib03 <mdsaquib980147@gmail.com>
2024-09-04 09:27:27 +0000

    pdfkit removed

--- dailyDozeOfDSA--Client/src/components/JobCard/JobCard.jsx
+++ dailyDozeOfDSA--Client/src/components/JobCard/JobCard.jsx
@@ -7,7 +7,7 @@ function JobCard({ companyName, jobTitle, tags, applyLink, company }) {
     <div className={styles.jobCard}>
       <div className={styles.companyLogo}>
       <img
-          src={company?.companyLogo || "/googleLogo.jpeg"} 
+          src={company?.companyLogo || "/dummyLogo.png"} 
           alt={`${companyName} Logo`} 
           className={styles.logoImg}
         />
