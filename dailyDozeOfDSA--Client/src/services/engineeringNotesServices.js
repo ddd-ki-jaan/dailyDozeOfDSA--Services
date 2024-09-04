aeea9aad6f6c82f9c2cb23435d60d82c91a6bd6d
mdsaquib03 <mdsaquib980147@gmail.com>
2024-09-04 14:58:34 +0530

    merge with client and server

--- dailyDozeOfDSA--Client/src/services/engineeringNotesServices.js
+++ dailyDozeOfDSA--Client/src/services/engineeringNotesServices.js
@@ -43,51 +43,6 @@ export async function getEngineeringNoteById(noteId) {
   }
 }
 
-import axios from "axios";
-
-export async function getEngineeringNotes(categoryVal, offset, showSavedNotes) {
-  const encodedCategoryVal = encodeURIComponent(categoryVal);
-  try {
-    const response = await axios.get(
-      `/api/v1/notes/getEngineeringNotes?categoryVal=${encodedCategoryVal}&offset=${offset}&showSavedNotes=${showSavedNotes}`
-    );
-    return response;
-  } catch (error) {
-    throw error;
-  }
-}
-
-export async function getEngineeringNoteCategories() {
-  try {
-    const response = await axios.get(
-      `/api/v1/notes/getEngineeringNoteCategories`
-    );
-    return response;
-  } catch (error) {
-    throw error;
-  }
-}
-
-export async function getEngineeringNoteTags() {
-  try {
-    const response = await axios.get(`/api/v1/notes/getEngineeringNoteTags`);
-    return response;
-  } catch (error) {
-    throw error;
-  }
-}
-
-export async function getEngineeringNoteById(noteId) {
-  try {
-    const response = await axios.get(
-      `/api/v1/notes/getEngineeringNoteById/${noteId}`
-    );
-    return response;
-  } catch (error) {
-    throw error;
-  }
-}
-
 export async function getNotesUrlFromSlug(slug) {
   try {
     const response = await axios.get(
